0889619a52dfcd67386bab1732e91c20aa3ad130
jennifer <jenniferdiaz@outlook.cl>
2020-11-22 16:59:03 -0300

    arreglando errores sintaxis

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
-app.use(require('./routes/usuario'));
+app.use(require('./routes/usuario.js'));
 
 // respond with "hello world" when a GET request is made to the homepage
 /*app.get('/', function(req, res) {
@@ -23,6 +23,6 @@ mongoose.connect('mongodb://localhost:27017/cafe', (err, res)=>{
 
 });
 
-app.listen(process.env.Port, () => {
-    console.log('Escuchando puerto: ', 3000);
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
